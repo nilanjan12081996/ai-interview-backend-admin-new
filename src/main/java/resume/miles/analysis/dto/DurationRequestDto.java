6ae00ae45cfd7020e4b2407f421590188bf59a93
koushikdev22 <koushik.journey.mondal@gmail.com>
2026-03-12 19:17:18 +0530

    new

--- src/main/java/resume/miles/analysis/dto/DurationRequestDto.java
+++ src/main/java/resume/miles/analysis/dto/DurationRequestDto.java
@@ -1,12 +1,12 @@
 package resume.miles.analysis.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DurationRequestDto {
     private String token;
     private String duration;
