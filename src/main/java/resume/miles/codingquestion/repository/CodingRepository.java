5179e08db437c8a379fee2d51cbd136ad52abd8b
koushikdev22 <koushik.journey.mondal@gmail.com>
2026-03-25 13:56:13 +0530

    new

--- src/main/java/resume/miles/codingquestion/repository/CodingRepository.java
+++ src/main/java/resume/miles/codingquestion/repository/CodingRepository.java
@@ -2,7 +2,9 @@ package resume.miles.codingquestion.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 import resume.miles.codingquestion.entity.CodingEntity;
 
+@Repository
 public interface CodingRepository extends JpaRepository<CodingEntity, Long>, JpaSpecificationExecutor<CodingEntity> {
 }
